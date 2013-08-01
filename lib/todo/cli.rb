60aae8825e3677f7dbf97d7d4a92d87978ed0bf4
vannell <jbh@mindgoo.be>
2013-08-01 02:07:53 +0200

    Add some commands alias

--- lib/todo/cli.rb
+++ lib/todo/cli.rb
@@ -4,13 +4,13 @@ module Todo
 class CLI
 	def run(args)
 		case args[0]
-		when "add"
+		when "add", "a"
 			TodoList.load.add args[1..-1]
-		when "list"
+		when "list", "ls", "l"
 			TodoList.load.list args[1..-1].join('_').to_sym
-		when "done"
+		when "done", "d"
 			TodoList.load.done *args[1..-1].map(&:to_i)
-		when "bump"
+		when "bump", "b"
 			if args.length == 2
 				TodoList.load.bump args[1].to_i
 			elsif args.length == 3
@@ -18,7 +18,7 @@ class CLI
 			end
 		when "init"
 			TodoList.init 
-		when "clear"
+		when "clear", "cl"
 			TodoList.load.clear
 		end
 	end
